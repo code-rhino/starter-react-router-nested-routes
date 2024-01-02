51b36ba02f4b253d44c5feb4947c431c50e1e199
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2024-01-02 15:44:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import UserProfile from "./UserProfile";
+import NoMatch from "./NoMatch";
+import BackButton from "./BackButton";
+import ForwardButton from "./ForwardButton";
+import GoHomeButton from "./GoHomeButton";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="navigation-buttons">
+        <BackButton />
+        <ForwardButton />
+        <GoHomeButton />
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/:userId" element={<UserProfile />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   );
 }
 
