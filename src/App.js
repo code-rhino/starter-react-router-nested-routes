ac97da6bed642422d3cf05dcf22421938356a27f
Tariq Hook <thook@Tariqs-MBP-2.hsd1.de.comcast.net>
2024-01-02 19:16:47 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import NoMatch from "./NoMatch";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import BackButton from "./BackButton";
 import ForwardButton from "./ForwardButton";
 import GoHomeButton from "./GoHomeButton";
-import User from "./User";
 import "./App.css";
+import RootRoutes from "./RootRoutes";
 
 function App() {
   return (
@@ -19,17 +16,9 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </div>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users/:userId" element={<User />}>
-          <Route path="posts" element={<p>Here are a list of the user’s posts</p>} />
-          <Route path="posts/:postId" element={<p>Here is a single post</p>} />
-        </Route>
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <RootRoutes />
     </Router>
+
   );
 }
 
