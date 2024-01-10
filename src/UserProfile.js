ee7960c182605ebb39e8c060e64d919aae378573
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2024-01-09 20:33:50 -0500

    updates

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function UserProfile({userId}) {
+function UserProfile() {
   const [user, setUser] = useState({});
   const { userId } = useParams()
   
