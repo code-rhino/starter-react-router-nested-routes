990dccf55c63312aa836fa137de6a1820ed4dd4c
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2024-01-09 20:22:48 -0500

    Complete

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function UserProfile({userId}) {
   const [user, setUser] = useState({});
-  const userId = "";
+  const { userId } = useParams()
   
   useEffect(() => {
     async function loadUser() {
